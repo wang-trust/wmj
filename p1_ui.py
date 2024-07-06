f4b982c0a0963d402dddb85825ce437df8a6a97b
goupi <2295165862@qq.com>
2024-07-06 13:14:00 +0800

    0.0.1.3 2024-07-06 完善UI

--- p1_ui.py
+++ p1_ui.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file 'p1.ui'
+# Form implementation generated from reading ui file 'c:\Users\goupi\Desktop\qt5\p1.ui'
 #
 # Created by: PyQt5 UI code generator 5.15.10
 #
